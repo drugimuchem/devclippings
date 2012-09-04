3d02fcf14bdacb7e588aa9b2e7828b9b0e1234f5
n8a <najbezatra@gmail.com>
2012-09-04 20:12:55 +0200

    multiline text stored in one div.text insead of one p per line

--- lib/html_sheet.rb
+++ lib/html_sheet.rb
@@ -3,11 +3,22 @@ module HtmlSheet
   def self.to_html(sheetStr)
     sheetHtml = []
     hStates   = {}
+    txtBuffer = ''
     (2..4).each{|i| hStates[i] = 0}
 
+    flushBuffer = lambda do
+      if txtBuffer =~ /\S/
+        sheetHtml.push('<div class="text">%s</div>' % txtBuffer.chomp) # remove last \n
+      end
+      txtBuffer.clear
+    end
+
     sheetStr.each_line do |line|
-      line.chomp!
+        #line.chomp!
       if line =~ /^\s*{#+}.+/
+
+        flushBuffer.call
+
         hParts  = line.lstrip.split('}',2)
         hText   = hParts[1].strip
         hLevel  = hParts[0].length
@@ -19,12 +30,13 @@ module HtmlSheet
         hNumber = hStates.keys.grep(1..hLevel).collect{|k| hStates[k]}.join('.')
 
         line = '<h%d id="%s-%s">%s</h%d>' % [ hLevel, hNumber, to_slug(hText), hText, hLevel ]
-      else
-        line = '<p>%s</p>' % line
+        sheetHtml.push(line)
+      elsif line =~ /\S/
+        txtBuffer << line
       end
-
-      sheetHtml.push(line)
     end
+
+    flushBuffer.call
 
     sheetHtml.join
   end
--- tests/html_sheet_test.rb
+++ tests/html_sheet_test.rb
@@ -6,8 +6,8 @@ class HtmlSheetTest < Test::Unit::TestCase
   def test_to_html_basic
     samples = [
       [ '', '' ],
-      [ "Deep dive", "<p>Deep dive</p>" ],
-      [ "Deep\ndive", "<p>Deep</p><p>dive</p>" ],
+      [ "Deep dive", '<div class="text">Deep dive</div>' ],
+      [ "Deep\ndive", "<div class=\"text\">Deep\ndive</div>" ],
     ]
     assert_to_html_in_out(samples)
   end
@@ -25,6 +25,12 @@ class HtmlSheetTest < Test::Unit::TestCase
       [ 
         "{#}First Chapter\n{#}Last Chapter", 
         '<h2 id="1-first-chapter">First Chapter</h2>' + 
+        '<h2 id="2-last-chapter">Last Chapter</h2>' 
+      ],
+      [ 
+        "{#}First Chapter\nSomething else\n{#}Last Chapter", 
+        '<h2 id="1-first-chapter">First Chapter</h2>' + 
+				'<div class="text">Something else</div>' +
         '<h2 id="2-last-chapter">Last Chapter</h2>' 
       ],
     ]
@@ -78,6 +84,18 @@ class HtmlSheetTest < Test::Unit::TestCase
         '<h2 id="2-second-chapter">Second Chapter</h2>' +
         '<h3 id="2.1-sub-chapter">Sub Chapter</h3>' +
         '<h4 id="2.1.1-mini-chapter">Mini Chapter</h4>' +
+        '<h3 id="2.2-next-sub-chapter">Next Sub Chapter</h3>' +
+        '<h4 id="2.2.1-another-mini-chapter">Another Mini Chapter</h4>'
+      ],
+      [ 
+        "{#}First Chapter\nYep  \n{##}Sub Chapter\n{#}Second Chapter\n{##}Sub Chapter\n{###}Mini Chapter\n   Exactly\n{##}Next Sub Chapter\n{###}Another Mini Chapter", 
+        '<h2 id="1-first-chapter">First Chapter</h2>' +
+				'<div class="text">Yep  </div>' +
+        '<h3 id="1.1-sub-chapter">Sub Chapter</h3>' +
+        '<h2 id="2-second-chapter">Second Chapter</h2>' +
+        '<h3 id="2.1-sub-chapter">Sub Chapter</h3>' +
+        '<h4 id="2.1.1-mini-chapter">Mini Chapter</h4>' +
+				'<div class="text">   Exactly</div>' +
         '<h3 id="2.2-next-sub-chapter">Next Sub Chapter</h3>' +
         '<h4 id="2.2.1-another-mini-chapter">Another Mini Chapter</h4>'
       ],
