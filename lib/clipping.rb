0f57766bf8e260086b999b2267a974e8f90f844d
ps539 <ps539@ps539.(none)>
2012-10-24 11:32:03 +0200

    no db credentials in code

--- lib/clipping.rb
+++ lib/clipping.rb
@@ -3,9 +3,7 @@ require 'dm-migrations'
 require './lib/markeline'
 
 # use env variable DM_SETUP for custom DB for DataMapper 
-dm_setups = Hash.new('mysql://root@localhost/chtshtdb');
-dm_setups['vipserv'] = 'mysql://magineo_chtshtu:321Jebud!!!@mysql-573360.vipserv.org/magineo_chtsht'
-DataMapper.setup(:default,dm_setups[ENV['DM_SETUP']])
+DataMapper.setup( :default, ENV['DB_URL'] || 'mysql://root@localhost/chtshtdb' )
 
 class Clipping
   include DataMapper::Resource
